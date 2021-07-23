37122a8a35fd7a3ae9824d0160844910aa666ad3
Jeremy Swafford <jeremy@swafford.co>
2021-07-23 16:02:14 -0400

    build out request url

--- src/service.ts
+++ src/service.ts
@@ -1,7 +1,17 @@
+import { CardRequestParam } from "./types"
+
 const baseURL = "https://api.magicthegathering.io/v1"
 
-export const fetchCards = async <T>() => {
-  const result = await fetch(baseURL+"/cards")
-  const temp = await result.json()
-  return temp //as unknown as T
+
+export const fetchCards = async (request: CardRequestParam) => {
+  const params = buildParameters(request)
+  const response = await fetch(`${baseURL}/cards${params}`)
+  return (await response.json()).cards}
+
+const buildParameters = (request: CardRequestParam) => {
+  const params = Object.entries(request).map(([key, value]) => {
+    return `${key}=${value}`
+  })
+
+  return params.length > 0 ? `?${params.join("&")}` : ""
 }
