dd8e49758e5e76fcdf00662ff84539dc5f0dd36b
Jeremy Swafford <jeremy@swafford.co>
2021-07-23 20:23:14 -0400

    add onClick to Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { MouseEventHandler } from 'react'
 import { CardDTO } from '../types'
 
-const Card = (props: Partial<CardDTO>) => {
-  const hasImage = !!props.imageUrl
+interface props {
+  cardProps: Partial<CardDTO>
+  onClick?: () => void
+}
+
+const Card = ({cardProps, onClick}: props) => {
+  const hasImage = !!cardProps.imageUrl
   return (
-    <div className={`m-1 w-32 ${!hasImage && "border-2 rounded-lg h-46"}`}>
-      {hasImage ? <img src={props?.imageUrl} alt="card art" /> : props?.name}
+    <div 
+      className={`m-1 w-32 ${!hasImage && "border-2 rounded-lg h-46"}`}
+      onClick={() => onClick && onClick()}
+    >
+      {hasImage
+            ? 
+            <img 
+              src={cardProps?.imageUrl}
+              alt="card art"
+            />
+        : cardProps?.name}
     </div>
   )
 }
